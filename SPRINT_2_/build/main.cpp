82545a4b9a643424bf84c502e7ac4401b461e01c
Taki Eddine Mekhalfa <ft_mekhalfa@esi.dz>
2019-03-12 00:06:53 +0100

    Update main.cpp

--- SPRINT_2_/build/main.cpp
+++ SPRINT_2_/build/main.cpp
@@ -3,7 +3,6 @@
 #include "antlr4-runtime.h"
 #include "Sprint_2_Lexer.h"
 #include "Sprint_2_Parser.h"
-#include "Visitor.h"
 
 using namespace antlr4;
 
@@ -15,7 +14,6 @@ int main(int argc, const char* argv[]) {
   Sprint_2_Parser parser(&tokens);
 
   tree::ParseTree *tree = parser.prg();
-  //Visitor visitor;
-  //visitor.visit(tree);
+
   return 0;
 }
