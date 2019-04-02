792e94a572d7392a0dec7fb44fc8eb89e8ca3dce
Taki Eddine MEKHALFA <ft_mekhalfa@esi.dz>
2019-04-02 18:51:11 +0200

    90% of different types of assignement

--- sprint/build/sprintLexer.h
+++ sprint/build/sprintLexer.h
@@ -15,10 +15,10 @@ public:
     T__0 = 1, T__1 = 2, T__2 = 3, T__3 = 4, T__4 = 5, T__5 = 6, T__6 = 7, 
     T__7 = 8, T__8 = 9, T__9 = 10, T__10 = 11, T__11 = 12, T__12 = 13, T__13 = 14, 
     T__14 = 15, T__15 = 16, T__16 = 17, T__17 = 18, T__18 = 19, T__19 = 20, 
-    T__20 = 21, T__21 = 22, TYPE = 23, ID = 24, INT = 25, PLUS_MINUS = 26, 
-    LT_GT = 27, EQ_NEQ = 28, LETTER = 29, DIGIT = 30, UNDER_SCORE = 31, 
-    CHAR = 32, INLINE_COMMENT = 33, MULTILINE_COMMENT = 34, INCLUDE = 35, 
-    WS = 36
+    T__20 = 21, TYPE = 22, ID = 23, INT = 24, PLUS_MINUS = 25, LT_GT = 26, 
+    EQUAL_ASSIGN = 27, OP_ASSIGN = 28, EQ_NEQ = 29, LETTER = 30, DIGIT = 31, 
+    UNDER_SCORE = 32, CHAR = 33, INLINE_COMMENT = 34, MULTILINE_COMMENT = 35, 
+    INCLUDE = 36, WS = 37
   };
 
   sprintLexer(antlr4::CharStream *input);
