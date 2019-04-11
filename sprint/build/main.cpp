1fc2a3cc721dd910300190265f45839788dc0d8c
Catalin <catalin.gangalic@insa-lyon.fr>
2019-04-11 15:52:01 +0200

    Added reading without cat

--- sprint/build/main.cpp
+++ sprint/build/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 #include "antlr4-runtime.h"
 #include "sprintLexer.h"
@@ -7,9 +8,44 @@
 
 using namespace antlr4;
 
+/*
+  Function that based on file name gets the whole program in a string
+*/
+std::string getProgram(string fileName){
+  ifstream f(fileName);
+  std::string str;
+  std::string fContent;
+  while (std::getline(f,str)){
+    fContent+=(str+"\n");
+  }
+  f.close();
+  return fContent;
+}
+
 int main(int argc, const char* argv[]) {
 
-  ANTLRInputStream input(std::cin);
+  // finding the file name from the main arguments
+  std::string fileName="";
+  for (int i=1; i<argc; i++){
+    if(argv[i][0]!='-'){
+      fileName=argv[i];
+      break;
+    }
+  }
+
+  if(!fileName.length()){
+    cerr << "[-] No file path given"<<endl;
+    return 0;
+  }
+
+  // getting program content and checking for path and emptiness
+  std::string fContent = getProgram(fileName);
+  if(!fContent.length()){
+    cerr << "[-] Wrong file path or empty file"<<endl;
+    return 0;
+  }
+
+  ANTLRInputStream input(fContent);
   sprintLexer lexer(&input);
   CommonTokenStream tokens(&lexer);
   sprintParser parser(&tokens);
@@ -18,8 +54,8 @@ int main(int argc, const char* argv[]) {
     cerr << "[-] Program is syntactically incorrect, no file has been generated" << endl;
   }
   else{   
-      Visitor visitor;
-      visitor.visit(tree);
+    Visitor visitor;
+    visitor.visit(tree);
   }
-return 0;
+  return 0;
 };
