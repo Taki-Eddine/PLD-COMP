e95136d4e469bd234a843ff6f3be981d28d406d0
Taki Eddine Mekhalfa <ft_mekhalfa@esi.dz>
2019-04-11 16:01:26 +0200

    Update main.cpp

--- sprint/build/main.cpp
+++ sprint/build/main.cpp
@@ -1,16 +1,51 @@
 #include <iostream>
+#include <fstream>
 
 #include "antlr4-runtime.h"
 #include "sprintLexer.h"
 #include "sprintParser.h"
 #include "Visitor.h"
-#include "ErrorsChecker.h"
 
 using namespace antlr4;
 
+/*
+  Function that based on file name gets the whole program in a string
+*/
+std::string getProgram(string fileName){
+  ifstream f(fileName);
+  std::string str;
+  std::string fContent;
+  while (std::getline(f,str)){
+    fContent+=(str+"\n");
+  }
+  f.close();
+  return fContent;
+}
+
 int main(int argc, const char* argv[]) {
 
-  ANTLRInputStream input(std::cin);
+  // finding the file name from the main arguments
+  std::string fileName="";
+  for (int i=1; i<argc; i++){
+    if(argv[i][0]!='-'){
+      fileName=argv[i];
+      break;
+    }
+  }
+
+  if(!fileName.length()){
+    cerr << "[-] No file path given"<<endl;
+    return 0;
+  }
+
+  // getting program content and checking for path and emptiness
+  std::string fContent = getProgram(fileName);
+  if(!fContent.length()){
+    cerr << "[-] Wrong file path or empty file"<<endl;
+    return 0;
+  }
+
+  ANTLRInputStream input(fContent);
   sprintLexer lexer(&input);
   CommonTokenStream tokens(&lexer);
   sprintParser parser(&tokens);
@@ -19,10 +54,8 @@ int main(int argc, const char* argv[]) {
     cerr << "[-] Program is syntactically incorrect, no file has been generated" << endl;
   }
   else{   
-    ErrorsChecker errorsChecker;
-    errorsChecker.visit(tree);
-    //Visitor visitor;
-    //visitor.visit(tree);
+    Visitor visitor;
+    visitor.visit(tree);
   }
-return 0;
+  return 0;
 };
